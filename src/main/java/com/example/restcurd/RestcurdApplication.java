42c12f3b6a67a4ee091215c461d6158833373a76
Pampa Aich <pampa.aich@ingrammicro.com>
2020-07-20 11:38:32 +0530

    test

---
+++ src/main/java/com/example/restcurd/RestcurdApplication.java
@@ -0,0 +1,14 @@
+package com.example.restcurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcurdApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+        SpringApplication.run(RestcurdApplication.class, args);
+    }
+
+}
